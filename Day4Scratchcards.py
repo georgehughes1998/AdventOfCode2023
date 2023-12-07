1fc036e7efe2db8569857f32885f5afe962cdf5b
George Hughes <ghughes@podfather.com>
2023-12-07 18:19:20 +0000

    Refactored testing framework

--- Day4Scratchcards.py
+++ Day4Scratchcards.py
@@ -7,5 +7,3 @@ class Solver:
     def solvePart2(self, i):
         return (lambda b: (f:=lambda a, m: m[0] + f(a[1:], [m[1:][v] + m[0]*(v < a[0]) for v in range(len(a)-1)]) if a else 0)(b, [1 for _ in range(len(b))]))([(lambda w, a: sum([n in w for n in a]))(*[re.match("Card +\d+: ((?: *\d+ ?)+)\|((?: *\d+ ?)+)", line)[n].split() for n in [1, 2]]) for line in i.splitlines()])
 
-
-# (lambda b: (f:=lambda a, m: m[0] + f(a[1:], [m[1:][v] + m[0]*(v < a[0]) for v in range(len(a)-1)]) if a else 0)(b, [1 for _ in range(len(b))]))
--- test_framework.py
+++ test_framework.py
@@ -1,65 +1,51 @@
 import unittest
 import Day1Trebuchet, Day4Scratchcards
 
-class Day1TrebuchetTest(unittest.TestCase):
-    def test_part1_sample(self):
-        with open("day1-1sample.txt", "r") as f:
-            i = f.read()
-        
-        actual = Day1Trebuchet.Solver().solvePart1(i)
-        expected = 142
-        self.assertEqual(expected, actual)
-
-    def test_part1_puzzle_input(self): 
-        with open("day1.txt", "r") as f:
-            i = f.read()
-        
-        actual = Day1Trebuchet.Solver().solvePart1(i)
-        print("Day 1 Answer 1:", actual)
-
-    def test_part2_sample(self):
-        with open("day1-2sample.txt", "r") as f:
-            i = f.read()
-        
-        actual = Day1Trebuchet.Solver().solvePart2(i)
-        expected = 281
-        self.assertEqual(expected, actual)
-
-    def test_part2_puzzle_input(self): 
-        with open("day1.txt", "r") as f:
-            i = f.read()
-        
-        actual = Day1Trebuchet.Solver().solvePart2(i)
-        print("Day 1 Answer 2:", actual)
 
 
-class Day4ScratchcardsTest(unittest.TestCase):
-    def test_part1_sample(self):
-        with open("day4sample.txt", "r") as f:
-            i = f.read()
+class SolverTestCases:       
+    class SolverTestCasesBase(unittest.TestCase):
+        @classmethod
+        def setUpClass(self, solver, day, part1SampleExpected, part2SampleExpected):
+            self.solver = solver
+            self.day = day
+            self.part1SampleExpected = part1SampleExpected
+            self.part2SampleExpected = part2SampleExpected
 
-        actual = Day4Scratchcards.Solver().solvePart1(i)
-        expected = 13
-        self.assertEqual(expected, actual)
+        def _test_sample(self, part, expected):
+            with open(f"input/day{self.day}-{part}sample.txt", "r") as f:
+                i = f.read()
 
-    def test_part2_sample(self):
-        with open("day4sample.txt", "r") as f:
-            i = f.read()
+            actual = self.solver.solvePart1(i) if part == 1 else self.solver.solvePart2(i)
+            self.assertEqual(expected, actual)
 
-        actual = Day4Scratchcards.Solver().solvePart2(i)
-        expected = 30
-        self.assertEqual(expected, actual)
+        def _test_puzzle_input(self, part):
+            with open(f"input/day{self.day}.txt", "r") as f:
+                i = f.read()
 
-    def test_part1_puzzle_input(self):
-        with open("day4.txt", "r") as f:
-            i = f.read()
+            answer = self.solver.solvePart1(i) if part == 1 else self.solver.solvePart2(i)
+            print(f"Day {self.day} Part {part}: {answer}")
 
-        actual = Day4Scratchcards.Solver().solvePart1(i)
-        print("Day 4 Answer 1:", actual)
+        def test_part1_sample(self):
+            self._test_sample(1, self.part1SampleExpected)
 
-    def test_part2_puzzle_input(self):
-        with open("day4.txt", "r") as f:
-            i = f.read()
+        def test_part2_sample(self):
+            self._test_sample(2, self.part2SampleExpected)
 
-        actual = Day4Scratchcards.Solver().solvePart2(i)
-        print("Day 4 Answer 2:", actual)
+        def test_part1_puzzle_input(self):
+            self._test_puzzle_input(1)
+
+        def test_part2_puzzle_input(self):
+            self._test_puzzle_input(2)
+
+
+class Day4ScratchcardsTest(SolverTestCases.SolverTestCasesBase):
+    @classmethod
+    def setUpClass(self):
+        super().setUpClass(Day4Scratchcards.Solver(), 4, 13, 30)
+
+
+class Day1TrebuchetTest(SolverTestCases.SolverTestCasesBase):
+    @classmethod
+    def setUpClass(self):
+        super().setUpClass(Day1Trebuchet.Solver(), 1, 142, 281)
